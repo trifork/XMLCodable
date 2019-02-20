e0ccc27001197806a1d204ae71a098bb5c22760c
Kim de Vos <kdv@trifork.com>
2019-02-20 15:05:40 +0100

    Moved files from https://github.com/kimdv/XMLParsing to Trifork repo

---
+++ Source/XMLCodable.h
@@ -0,0 +1,19 @@
+//
+//  XMLCodable.h
+//  XMLCodable
+//
+//  Created by Kim de Vos on 20/02/2019.
+//  Copyright © 2019 Trifork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMLCodable.
+FOUNDATION_EXPORT double XMLCodableVersionNumber;
+
+//! Project version string for XMLCodable.
+FOUNDATION_EXPORT const unsigned char XMLCodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLCodable/PublicHeader.h>
+
+
